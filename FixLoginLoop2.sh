d2e9f64803c216c92d9f3dbc574cb807cee17983
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-12-22 18:30:54 -0300

    Atualizar o

---
+++ FixLoginLoop2.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade
+dpkg –configure -a
+sudo apt-get install cinnamon-control-center-data
+gdm3 –version
+apt-get –reinstall install gdm3
+apt-get –reinstall install gnome
+apt-get –reinstall install gnome-shell
+dpkg-reconfigure gdm3
+reboot
